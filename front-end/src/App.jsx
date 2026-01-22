8c94d0fa10828b23db590019476424e1823f8777
enricogatta <enricogatta69@gmail.com>
2026-01-22 19:19:22 +0100

    fetch libri e generi

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,6 +3,32 @@ import './App.css';
 
 function App() {
   const [libri, setLibri] = useState([]);
+  const [generi, setGeneri] = useState([]);
+
+  useEffect(() => {
+    fetchLibri();
+    fetchGeneri();
+  }, []);
+
+  const fetchLibri = async () => {
+    try {
+      const res = await fetch('http://127.0.0.1:5000/api/libri');
+      const data = await res.json();
+      setLibri(data);
+    } catch (error) {
+      console.error("Errore di connessione al backend:", error);
+    }
+  };
+
+  const fetchGeneri = async () => {
+    try {
+      const res = await fetch('http://127.0.0.1:5000/api/generi');
+      const data = await res.json();
+      setGeneri(data);
+    } catch (error) {
+      console.error("Errore nel recupero dei generi:", error);
+    }
+  };
 
   return (
     <div className="page-wrapper">
@@ -10,7 +36,7 @@ function App() {
         <header className="header-app">
           <h1>Gestione Libreria</h1>
         </header>
-        {/* Il resto verrà aggiunto nei prossimi commit */}
+        
       </div>
     </div>
   );
